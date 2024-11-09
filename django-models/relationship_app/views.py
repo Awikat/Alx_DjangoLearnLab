db7d4a16a0f375453d94544d6f1d23b98789f89c
maurice_awika <maurice@awikat.awika>
2024-11-09 14:17:51 +0300

    This is my sixth commit.

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,35 +1,94 @@
-
-# relationship_app/views.py
-
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book, Library  # Add the import for Library model
+from .models import Book
+from .models import Library
+from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.http import HttpResponseForbidden
+from .models import UserProfile
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
 
-# Function-based view for listing books
 def list_books(request):
-    """
-    View to list all books stored in the database, displaying each title and author.
-    """
-    books = Book.objects.all()  # Retrieve all books from the database
-    context = {
-        'books': books
-    }
-    return render(request, 'relationship_app/list_books.html', context)  # Ensure correct template path
+	books = Book.objects.all()
+	return render(request, "relationship_app/list_books.html", {'books': books})
 
-# Class-based view for displaying library details
 class LibraryDetailView(DetailView):
-    """
-    View to display details for a specific library, listing all books available in that library.
-    """
-    model = Library  # The model for the view is Library
-    template_name = 'relationship_app/library_detail.html'  # Define the template to use
-    context_object_name = 'library'  # The object will be accessible as 'library' in the template
+	model = Library
+	template_name = 'relationship_app/library_detail.html'
+	context_object_name = 'library'
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect('login')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 
-    def get_context_data(self, **kwargs):
-        """
-        Override the default context to add all books for the current library.
-        """
-        context = super().get_context_data(**kwargs)
-        # Get all books related to the library (many-to-many relationship)
-        context['books'] = self.object.books.all()  # 'self.object' is the current library instance
-        return context
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+	
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author_id = request.POST.get('author')
+        publication_year = request.POST.get('publication_year')
+
+        book = Book(title=title, author_id=author_id, publication_year=publication_year)
+        book.save()
+        return redirect('book_list')  
+    return render(request, 'relationship_app/add_book.html')
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author_id = request.POST.get('author')
+        book.publication_year = request.POST.get('publication_year')
+        book.save()
+        return redirect('book_list')  
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')  
+
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
