fe1ebfb6e1e0869a623fd81854bef22c14e47957
maurice_awika <maurice@awikat.awika>
2024-12-08 23:16:29 +0300

    This is Maurice Awika fifth commit.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,10 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from .models import Post
+from .models import Comment
+from taggit.forms import TagWidget
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -14,5 +16,15 @@ class CustomUserCreationForm(UserCreationForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
