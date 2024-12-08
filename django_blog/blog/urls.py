a02adb3c5e56add553b9b9b4bb90595ba11d036c
maurice_awika <maurice@awikat.awika>
2024-12-08 23:06:43 +0300

    This is Maurice Awika fourth commit.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
-     path('posts/', views.PostListView.as_view(), name='post_list'),  # List posts
+    path('posts/', views.PostListView.as_view(), name='post_list'),  # List posts
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),  # Post detail
     path('posts/new/', views.PostCreateView.as_view(), name='post_create'),  # Create post
     path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),  # Edit post
