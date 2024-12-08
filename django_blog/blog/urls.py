2474d4917ff2d09b8345cb6b33bd4224557cfbe5
maurice_awika <maurice@awikat.awika>
2024-12-08 22:43:02 +0300

    This is Maurice Awika third commit.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views  # Import the views module
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),  # Registration URL
-    path('profile/', views.profile, name='profile'),  # Profile URL
+    path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
 
