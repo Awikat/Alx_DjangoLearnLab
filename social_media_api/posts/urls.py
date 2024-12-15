c6a70b8cdb3a91b39804e736b7c5eaf8453c4bde
maurice awika <awika@debian.awikat>
2024-12-15 22:36:37 +0300

    This is maurice Awika Posts model ninth commit.

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,19 +1,14 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
-from .views import FeedView
+from .views import PostViewSet, CommentViewSet, FeedView, LikePostView, UnlikePostView
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='post')
+router.register('comments', CommentViewSet, basename='comment')
+
 
 urlpatterns = [
     path('feed/', FeedView.as_view(), name='feed'),
-]
-
-# Create a router and register the viewsets
-router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
-router.register(r'comments', CommentViewSet, basename='comment')
-
-# Include the router URLs
-urlpatterns = [
-    path('', include(router.urls)),
-]
-
+    path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
+] + router.urls
